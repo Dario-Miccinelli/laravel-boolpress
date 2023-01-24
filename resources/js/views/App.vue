a7ca32edad3742d9217d89b7d8b88772b973ca2a
Dario-Miccinelli <poduzzo@hotmail.com>
2023-01-24 17:43:44 +0100

    Collegato Vue

---
+++ resources/js/views/App.vue
@@ -0,0 +1,47 @@
+<template>
+
+    <div>
+
+        <nav class="navbar bg-body-tertiary">
+
+            <a class="navbar-brand" href="#">
+                <img class="ps-2 ms-1" src="https://cdn.icon-icons.com/icons2/2108/PNG/512/vue_icon_130791.png" alt="Bootstrap" width="40" height="34">
+                Vue.Js
+            </a>
+
+
+        </nav>
+
+
+
+
+        <div class="container-fluid pt-3">
+            <h3 class="">Vue Works!</h3>
+
+        </div>
+
+
+    </div>
+
+
+
+
+
+
+</template>
+
+
+<script>
+
+export default {
+    name: "App",
+
+
+    // futuri components
+    components: {
+
+    }
+}
+
+
+</script>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,8 +1,24 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Guest Page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+</head>
+
+<body class="">
+
+    {{-- vue div root app --}}
+    <div id="root">
 
 
 
-@section('content')
-    <h2 class="">Laravel Auth</h2>
-    <p>Guest page</p>
-@endsection
+    </div>
+
+    <script src="{{ asset('js/app.js')}}"></script>
+</body>
+
+</html>
